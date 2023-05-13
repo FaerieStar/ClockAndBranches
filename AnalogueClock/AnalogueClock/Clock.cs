a802e469a1f465cdb395c84dfbaf0c7d6979c605
Stella Mariz Palen <stellamariz.palen@cit.edu>
2023-05-13 10:13:23 +0800

    First commit

---
+++ AnalogueClock/AnalogueClock/Clock.cs
@@ -0,0 +1,34 @@
+﻿namespace AnalogueClock
+{
+    class Clock
+    {
+        public int calculate(int hour, int minute)
+        {
+            if(hour < 0 || hour > 12 || minute < 0 || minute > 60)
+            {
+                Console.WriteLine("Input invalid.");
+                return -1;
+            }
+            int hourAngle = (minute / 60 * hour % 12) * 30;
+            int minuteAngle = minute * 6;
+
+            int angle = Math.Abs(hourAngle - minuteAngle);
+            return angle > 180 ? (360 - angle) : angle;
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Clock clock = new Clock();
+            int hour, minute;
+
+            Console.WriteLine("Please input hours: ");
+            hour = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please input minutes: ");
+            minute = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(clock.calculate(hour, minute) + "°");
+        }
+    }
+}
---
+++ Branches/Branches/Branch.cs
@@ -0,0 +1,83 @@
+﻿namespace BranchProgram
+{
+    class Branch
+    {
+        public int num;
+        public Branch? left;
+        public Branch? right;
+        public Branch(int num)
+        {
+            this.num = num;
+            left = null;
+            right = null;
+        }
+    }
+    class BranchList
+    {
+        Branch? root;
+        List<Branch> branches;
+        public BranchList()
+        {
+            root = null;
+        }
+        public void Add(int num)
+        {
+            root = AddRecur(root, num);
+        }
+        public Branch AddRecur(Branch curr, int num)
+        {
+            if (curr == null)
+            {
+                return new Branch(num);
+            }
+
+            if(num < curr.num)
+            {
+                curr.left = AddRecur(curr.left, num);
+            }
+            else if (num > curr.num)
+            {
+                curr.right = AddRecur(curr.right, num);
+            }
+            else
+            {
+                return curr;
+            }
+            return curr;
+        }
+        public int Depth()
+        {
+            return DepthRecur(root);
+        }
+        public int DepthRecur(Branch branch)
+        {
+            if(branch == null)
+            {
+                return 0;
+            }
+            int depthLeft = DepthRecur(branch.left);
+            int depthRight = DepthRecur(branch.right);
+
+            return depthLeft > depthRight ? (depthLeft + 1) : (depthRight + 1);
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            BranchList branches = new BranchList();
+            char option = 'y';
+            int num;
+            while(option == 'y')
+            {
+                Console.WriteLine("Input number to add: ");
+                num = Convert.ToInt32(Console.ReadLine());
+                branches.Add(num);
+
+                Console.WriteLine("Add new numbers? [y/n]");
+                option = Convert.ToChar(Console.ReadLine());
+            }
+            Console.WriteLine("Depth is: " + branches.Depth());
+        }
+    }
+}
